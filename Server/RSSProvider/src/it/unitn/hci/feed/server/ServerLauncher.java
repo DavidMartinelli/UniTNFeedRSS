a612c74558418496183a30f2e7dfc6133b812422
Gianluca Bonacin <bonacin@outlook.com>
2013-11-14 21:13:09 +0100

    fixed class path and implemented server launcher

--- Server/RSSProvider/src/it/unitn/hci/feed/server/ServerLauncher.java
+++ Server/RSSProvider/src/it/unitn/hci/feed/server/ServerLauncher.java
@@ -1,10 +1,20 @@
 package it.unitn.hci.feed.server;
 
+import java.net.InetAddress;
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+
 public class ServerLauncher
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        // TODO implement
+        final String port;
+        if (args.length == 1) port = args[0];
+        else port = "8080";
+
+        final String url = "http://" + InetAddress.getLocalHost().getHostAddress() + ":" + port + "/";
+
+        HttpServerFactory.create(url).start();
+        System.out.println("server starts on " + url + "\n [kill the process to exit]");
     }
 }
