5db4f52136ae3b4099247f182efcd81cb24ee27a
David Martinelli <polenta92@gmail.com>
2013-11-20 00:05:11 +0100

    fragment menu added

--- Android/RSSReader/src/it/unitn/hci/feed/android/MainActivity.java
+++ Android/RSSReader/src/it/unitn/hci/feed/android/MainActivity.java
@@ -13,20 +13,28 @@ import it.unitn.hci.feed.android.utils.RSSAsyncReader;
 import it.unitn.hci.feed.common.models.Course;
 import it.unitn.hci.feed.common.models.Feed;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.app.Activity;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 
-public class MainActivity extends Activity
+public class MainActivity extends FragmentActivity
 {
     private ExpandableListAdapter mCoursesAdapter;
     private ExpandableListView mCoursesList;
 
     private Map<String, List<Feed>> mCourses;
+    private ImageView btnMenu;
 
 
     @Override
@@ -36,6 +44,7 @@ public class MainActivity extends Activity
         setContentView(R.layout.activity_main);
 
         mCoursesList = (ExpandableListView) findViewById(R.id.lstCourses);
+        btnMenu = (ImageView) findViewById(R.id.btnMenu);
 
         final ProgressDialog dialog = new ProgressDialog(this);
         dialog.setTitle("Loading feeds");
@@ -70,6 +79,8 @@ public class MainActivity extends Activity
                 mCoursesList.setAdapter(mCoursesAdapter);
             }
         });
+
+        btnMenu.setOnClickListener(onBtnMenuClicked(getSupportFragmentManager()));
     }
 
     private final static OnClickListener mCourseClickedListener = new OnClickListener()
@@ -77,6 +88,81 @@ public class MainActivity extends Activity
         @Override
         public void onClick(View v)
         {
+        }
+    };
+
+
+    private static OnClickListener onBtnMenuClicked(final FragmentManager fm)
+    {
+        return new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                DialogFragment d = new DialogFragment()
+                {
+
+                    TextView btnManageFeeds;
+                    TextView btnShowAllFeeds;
+                    TextView btnEnableNotification;
+
+
+                    @Override
+                    public View onCreateView(LayoutInflater inflater, android.view.ViewGroup container, android.os.Bundle savedInstanceState)
+                    {
+                        View rootView = inflater.inflate(R.layout.fast_menu_layout, container, false);
+
+                        btnManageFeeds = (TextView) rootView.findViewById(R.id.btnManageFeeds);
+                        btnShowAllFeeds = (TextView) rootView.findViewById(R.id.btnShowAllFeeds);
+                        btnEnableNotification = (TextView) rootView.findViewById(R.id.btnEnableNotification);
+
+                        btnManageFeeds.setOnClickListener(mOnManageFeedsCkicked);
+                        btnShowAllFeeds.setOnClickListener(mOnShowAllFeedsCkicked);
+                        btnEnableNotification.setOnClickListener(mOnEnableNotificationCkicked);
+
+                        return rootView;
+                    }
+
+
+                    @Override
+                    public Dialog onCreateDialog(Bundle savedInstanceState)
+                    {
+                        Dialog dialog = super.onCreateDialog(savedInstanceState);
+                        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                        return dialog;
+                    }
+
+                };
+
+                d.show(fm, "123");
+            }
+        };
+    }
+
+    private static final OnClickListener mOnManageFeedsCkicked = new OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+
+        }
+    };
+
+    private static final OnClickListener mOnShowAllFeedsCkicked = new OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+
+        }
+    };
+
+    private static final OnClickListener mOnEnableNotificationCkicked = new OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+
         }
     };
 
