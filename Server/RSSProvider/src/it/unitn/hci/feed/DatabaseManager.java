409abd9b8c9baa5b6f703e55c502c1abed002a2e
Francesco <francesco@Francescos-MacBook-Pro.local>
2013-11-22 10:57:49 +0100

    added a lot of stuff (see payload)

--- Server/RSSProvider/src/it/unitn/hci/feed/DatabaseManager.java
+++ Server/RSSProvider/src/it/unitn/hci/feed/DatabaseManager.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.Set;
 import it.unitn.hci.feed.common.models.Course;
+import it.unitn.hci.feed.common.models.Department;
 import it.unitn.hci.feed.common.models.Feed;
 import it.unitn.hci.utils.ColourUtils;
 import it.unitn.hci.utils.TODOException;
@@ -58,7 +59,7 @@ public class DatabaseManager
 
     private final static String GET_ALIASES_BY_COURSE_NAME = "SELECT " + COLUMN_ALIAS_VALUE + " FROM " + TABLE_ALIASES + " JOIN " + TABLE_COURSES + " ON " + COLUMN_ALIAS_FK_COURSE + "=" + COLUMN_COURSE_ID + " WHERE " + COLUMN_COURSE_NAME + "=?";
     private final static String GET_ALL_COURSES = "SELECT * FROM " + TABLE_COURSES;
-    private final static String GET_ALL_FEEDS = "SELECT * FROM " + TABLE_FEEDS + " JOIN " + TABLE_COURSES + " ON " +  TABLE_FEEDS + "." + COLUMN_FEED_FK_COURSE + " = " + TABLE_COURSES + "." + COLUMN_COURSE_ID;
+    private final static String GET_ALL_FEEDS = "SELECT * FROM " + TABLE_FEEDS + " JOIN " + TABLE_COURSES + " ON " + TABLE_FEEDS + "." + COLUMN_FEED_FK_COURSE + " = " + TABLE_COURSES + "." + COLUMN_COURSE_ID;
     private final static String GET_COURSES_COLOURS_AND_NAMES = "SELECT " + COLUMN_COURSE_COLOUR + ", " + COLUMN_COURSE_NAME + " FROM " + TABLE_COURSES;
     private final static String GET_COURSES_NAME_BY_DEPARTMENT_NAME = "SELECT * FROM " + TABLE_COURSES + " JOIN " + TABLE_DEPARTMENTS_COURSES + " ON " + COLUMN_COURSE_ID + "=" + COLUMN_DEPARTMENT_COURSE_FK_COURSES + " JOIN " + TABLE_DEPARTMENTS + " JOIN " + COLUMN_DEPARTMENT_ID + "=" + COLUMN_DEPARTMENT_COURSE_FK_DEPARTMENTS + " WHERE " + COLUMN_DEPARTMENT_NAME + "=?";
     private final static String GET_FEEDS_BY_COURSE_NAME = "SELECT * FROM " + TABLE_FEEDS + " JOIN " + TABLE_COURSES + " ON " + COLUMN_COURSE_ID + "=" + COLUMN_FEED_FK_COURSE + " WHERE " + COLUMN_COURSE_NAME + "=?";
@@ -165,20 +166,21 @@ public class DatabaseManager
     }
 
 
-    public static List<String> getDepartments() throws Exception
+    public static List<Department> getDepartments() throws Exception
     {
-        List<String> deparmentNames = null;
+        List<Department> deparmentNames = null;
         Database db = null;
         try
         {
             db = Database.fromConnectionPool();
             ResultSet rs = db.executeQuery(GET_DEPARTMENTS_NAMES);
-            deparmentNames = new ArrayList<String>();
-            
-            while(rs.next()){
-                deparmentNames.add(rs.getString(COLUMN_DEPARTMENT_NAME));
+            deparmentNames = new ArrayList<Department>();
+
+            while (rs.next())
+            {
+                deparmentNames.add(new Department(rs.getString(COLUMN_DEPARTMENT_NAME)));
             }
-            
+
             return deparmentNames;
         }
         finally
@@ -186,7 +188,8 @@ public class DatabaseManager
             Database.close(db);
         }
     }
-    
+
+
     public static Set<String> getAliases(String courseName) throws Exception
     {
         Set<String> aliases = null;
@@ -196,18 +199,19 @@ public class DatabaseManager
             db = Database.fromConnectionPool();
             ResultSet rs = db.executeQuery(GET_ALIASES_BY_COURSE_NAME, courseName);
             aliases = new HashSet<String>();
-            
-            while(rs.next()){
+
+            while (rs.next())
+            {
                 aliases.add(rs.getString(COLUMN_ALIAS_VALUE));
             }
-            
+
             return aliases;
         }
         finally
         {
             Database.close(db);
         }
-   
+
     }
 
 
@@ -313,13 +317,13 @@ public class DatabaseManager
         Database db = null;
         Course course = null;
         Set<String> aliases = null;
-        
+
         try
         {
             db = Database.fromConnectionPool();
             ResultSet rs = db.executeQuery(GET_ALL_COURSES);
             courses = new ArrayList<Course>();
-            
+
             while (rs.next())
             {
                 aliases = getAliases(rs.getString(COLUMN_COURSE_NAME));
