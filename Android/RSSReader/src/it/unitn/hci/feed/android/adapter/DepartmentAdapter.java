88c20190b33dd85ca668648730e9c2c0b814fb06
Francesco <francesco@Francescos-MacBook-Pro.local>
2013-11-27 00:19:56 +0100

    better department support (client)

--- Android/RSSReader/src/it/unitn/hci/feed/android/adapter/DepartmentAdapter.java
+++ Android/RSSReader/src/it/unitn/hci/feed/android/adapter/DepartmentAdapter.java
@@ -1,6 +1,7 @@
 package it.unitn.hci.feed.android.adapter;
 
 import it.unitn.hci.feed.R;
+import it.unitn.hci.feed.common.models.Department;
 import java.util.List;
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -9,30 +10,31 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-public class DepartmentAdapter extends ArrayAdapter<String>
+public class DepartmentAdapter extends ArrayAdapter<Department>
 {
-    private List<String> mDepartments;
+    private List<Department> mDepartments;
     private Context mContext;
     private LayoutInflater mInflater;
 
 
-    public DepartmentAdapter(Context context, List<String> departments)
+    public DepartmentAdapter(Context context, List<Department> departments)
     {
         super(context, R.layout.department_chooser_item_layout, departments);
         mDepartments = departments;
         mContext = context;
         mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
-    
+
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
         if (convertView == null) convertView = mInflater.inflate(R.layout.department_chooser_item_layout, null);
         TextView departmentName = (TextView) convertView.findViewById(R.id.lblDepartmentName);
-        
-        departmentName.setText(mDepartments.get(position));
-        
-        return convertView;   
+
+        departmentName.setText(mDepartments.get(position).getName());
+
+        return convertView;
     }
 
 }
